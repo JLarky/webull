a1263881589d7741707cc586c32f6abf42464437
JLarky <jlarky@gmail.com>
2020-01-04 21:56:19 -0800

    try react-ga

--- weibull/src/components/FancyDiv.tsx
+++ weibull/src/components/FancyDiv.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import ReactGA from "react-ga";
+
+if (typeof window === "object") {
+  ReactGA.initialize("UA-5041583-10");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+  console.log(ReactGA);
+}
 
 const FancyDiv: React.FC = ({ children }) => {
   return <div style={{ border: '1px solid red' }}>{children}</div>
